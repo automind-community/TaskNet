296bce8ddcfdbda11e6fff25018b68b5d7eb7273
Ahmad Rauf <ahmadrauf.dev@gmail.com>
2026-08-19 17:50:09 +0500

    beta

--- src/tasknet.cpp
+++ src/tasknet.cpp
@@ -3,16 +3,48 @@
 
 
 using namespace std;
-
+void add_task();
+void show_task();
 int main()
 {
-    fstream task_file("taskdb.txt",ios::in);
-    if(!task_file)
+
+    bool is_menu_running = true;
+
+    cout<<"\n==============================\n";
+    cout<<"         TaskNet\n";
+    cout<<"==============================\n";
+
+    while(is_menu_running)
     {
-        cout<<"Error opening File"<<endl;
-    }else{
-        cout<<"Files opened successfully"<<endl;
+
+        int usrinp;
+        cout<<"Please Enter a selection\n1.Add Task\t2.Show Tasks\n";
+        cin>>usrinp;
+        switch(usrinp)
+        {
+            case 1:add_task();break;
+            case 2:show_task();break;
+            case 3:is_menu_running = false;break;
+            default:cout<<"Please Enter a valid selection";break;
+        }
     }
-    task_file.close();
+
+
 
 }
+
+void add_task()
+{
+    fstream add_file("filedb.txt",ios::in);
+    if(!add_file)
+    {
+        cout<<"Error Opening File"<<endl;
+    }
+
+}
+
+void show_task()
+{
+    
+}
+
