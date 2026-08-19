5d29e3d974cb8e7b0c95005f2bbfe4abc1f45cc0
Ahmad Rauf <ahmadrauf.dev@gmail.com>
2026-08-19 18:26:06 +0500

    beta

--- src/tasknet.cpp
+++ src/tasknet.cpp
@@ -33,13 +33,53 @@ int main()
 
 }
 
+enum Priority
+{
+    HIGH,
+    MEDIUM,
+    LOW
+};
+
 void add_task()
 {
-    fstream add_file("filedb.txt",ios::in);
+    string temp_task_name;
+    string temp_task_description;
+    Priority temp_task_priority;
+
+    bool is_priority = true;
+
+    fstream add_file("taskdb.txt",ios::out);
     if(!add_file)
     {
         cout<<"Error Opening File"<<endl;
     }
+
+    cout<<"Enter Task Name:\n";
+    cin>>temp_task_name;
+    cout<<"Enter Task description:\n";
+    cin.ignore();
+    getline(cin,temp_task_description);
+    while(is_priority)
+    {
+        int user_priotity_inp;
+        cout<<"Enter the priority of task\n1.HIGH\t2.MEDIUM\t3.LOW\n";
+        cin>>user_priotity_inp;
+        switch(user_priotity_inp)
+        {
+            case 1:temp_task_priority = Priority::HIGH;is_priority = false;break;
+            case 2:temp_task_priority = Priority::MEDIUM;is_priority = false;break;
+            case 3:temp_task_priority = Priority::LOW;is_priority = false;break;
+            default:cout<<"Please Enter a valid Selection!"<<endl;
+
+        }
+        
+    }
+
+    add_file<<temp_task_name<<"-"<<temp_task_description<<"-"<<temp_task_priority;
+
+
+    
+    
 
 }
 
